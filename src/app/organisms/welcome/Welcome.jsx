3327853176b96809e6fab9383e12a1f7cbec1aa0
aaron837 <aaron.rice@sjsu.edu>
2022-08-01 21:55:58 -0700

    Commit

--- src/app/organisms/welcome/Welcome.jsx
+++ src/app/organisms/welcome/Welcome.jsx
@@ -11,7 +11,7 @@ function Welcome() {
       <div>
         <img className="app-welcome__logo noselect" src={'https://cdn.discordapp.com/attachments/936339443361665066/1001342470002581615/nexus-icon.png'} alt="Nexus logo" />
         <Text className="app-welcome__heading" variant="h1" weight="medium" primary>Welcome to Nexus</Text>
-        <Text className="app-welcome__subheading" variant="s1">Yet another matrix client</Text>
+        <Text className="app-welcome__subheading" variant="s1">A unified messaging aggregate.</Text>
       </div>
     </div>
   );
